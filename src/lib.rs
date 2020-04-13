530e8758bf519a043de78519d9fe16dc61ee57da
Alex Pol <pa@green-g.ru>
2020-04-13 04:53:43 +0300

    some updates

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+use web_sys::*;
+
+mod gl_setup;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn say_hello_from_rust() {
+    log("Howdy!... from Rust =)")
+}
+
+#[wasm_bindgen]
+pub struct AmberSkyNet {
+    gl: WebGlRenderingContext,
+}
+
+#[wasm_bindgen]
+impl AmberSkyNet {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        console_error_panic_hook::set_once();
+        log("AmberSkyNet new");
+        let gl = gl_setup::initialize_webgl_context().unwrap();
+        Self {
+            gl,
+        }
+    }
+
+    // pub fn update(&mut self, _time: f32, _height: f32, _width: f32) {
+    //
+    // }
+    //
+    // pub fn render(&self) {
+    //
+    // }
+}
