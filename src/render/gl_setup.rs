1a77dbf508a8501286d5aad7236793b153b9ef09
Alex Pol <pa@green-g.ru>
2020-05-10 15:56:32 +0300

    it worked

--- src/render/gl_setup.rs
+++ src/render/gl_setup.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::JsCast;
 use wasm_bindgen::JsValue;
-use web_sys::*;
+use web_sys::window;
 use web_sys::WebGlRenderingContext as GL;
 
 use crate::logger::log;
@@ -9,12 +9,11 @@ pub fn get_webgl_context () -> Result<GL, JsValue> {
     log("get_webgl_context");
     let window: web_sys::Window = window().unwrap();
     let document: web_sys::Document = window.document().unwrap();
-    let element: web_sys::Element = document.get_element_by_id("rustCanvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = element.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
 
-    // let gl:WebGlRenderingContext = canvas.get_context("webgl")?.unwrap().dyn_into()?;
-    // пишу чтобы было понятнее потом %)
+    let element: web_sys::Element = document.get_element_by_id("rustCanvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = element.dyn_into()?;
+
     let gl_object: js_sys::Object = canvas.get_context("webgl")?.unwrap();
-    let gl_context:WebGlRenderingContext = gl_object.dyn_into()?;
+    let gl_context:GL = gl_object.dyn_into()?;
     Ok(gl_context)
 }
